7b2f38df3f71a06cfe4e2cffdec1fd1385d746a4
Roy Christian Pery <thercpery@gmail.com>
2023-01-10 21:02:50 +0800

    recreated App.js, index.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const title = 'Blog Post';
+  // const body = 'This is my blog post';
+	// const comments = [
+	// 	{ id: 1, text: 'Comment one' },
+	// 	{ id: 2, text: 'Comment two' },
+	// 	{ id: 3, text: 'Comment three' },
+	// ];
+
+	// const loading = false;
+	// const showComments = true;
+
+	// if(loading) return <h1>Loading...</h1>
+
+	// const commentBlock = (
+	// 	<div className="comments">
+	// 		<h3>Comments ({comments.length})</h3>
+	// 		<ul>
+	// 			{comments.map((comment, index) => (
+	// 				<li key={index}>{comment.text}</li>
+	// 			))}
+	// 		</ul>
+	// 	</div>
+	// );
+
+  // return (
+	// 	<div className='container'>
+  //     <h1>{title.toUpperCase()}</h1>
+  //     <p>{body}</p>
+
+	// 		{(showComments) && commentBlock}			
+  //   </div>
+  // )
+
+	return (
+		<>
+			<Header />
+			<div className="container">
+				<FeedbackItem/>
+			</div>
+		</>
+	)
 }
 
 export default App;
