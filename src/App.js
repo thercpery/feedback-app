0a42e818f3637aeeee975b2ec1992d6a6f603f27
Roy Christian Pery <thercpery@gmail.com>
2023-01-10 21:38:59 +0800

    imported feedback list and feedback data component to App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
-  // const title = 'Blog Post';
-  // const body = 'This is my blog post';
-	// const comments = [
-	// 	{ id: 1, text: 'Comment one' },
-	// 	{ id: 2, text: 'Comment two' },
-	// 	{ id: 3, text: 'Comment three' },
-	// ];
-
-	// const loading = false;
-	// const showComments = true;
-
-	// if(loading) return <h1>Loading...</h1>
-
-	// const commentBlock = (
-	// 	<div className="comments">
-	// 		<h3>Comments ({comments.length})</h3>
-	// 		<ul>
-	// 			{comments.map((comment, index) => (
-	// 				<li key={index}>{comment.text}</li>
-	// 			))}
-	// 		</ul>
-	// 	</div>
-	// );
-
-  // return (
-	// 	<div className='container'>
-  //     <h1>{title.toUpperCase()}</h1>
-  //     <p>{body}</p>
-
-	// 		{(showComments) && commentBlock}			
-  //   </div>
-  // )
+  const [feedback, setFeedback] = useState(FeedbackData);
 
 	return (
 		<>
 			<Header />
 			<div className="container">
-				<FeedbackItem/>
+				<FeedbackList feedback={feedback} />
 			</div>
 		</>
 	)
