19ab1c9ae65eebf3fb0c6947e49f6ccad8d53a6e
Roy Christian Pery <thercpery@gmail.com>
2023-01-11 21:42:05 +0800

    added handleDelete via prop drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 import { PropTypes } from 'prop-types';
+import Card from './shared/Card';
 
-const FeedbackItem = ({ item }) => {
-  const { rating, text } = item;
+const FeedbackItem = ({ item, handleDelete }) => {
+  const { id, rating, text } = item;
   return (
-    <div className='card'>
+    <Card>
       <div className="num-display">{rating}</div>
+      <button onClick={() => handleDelete(id)} className="close">
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">{text}</div>
-    </div>
+    </Card>
   )
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object,
+  item: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func
 };
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = ({ feedback }) => {
+const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -10,7 +10,10 @@ const FeedbackList = ({ feedback }) => {
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem 
+        key={item.id} 
+        item={item} 
+        handleDelete={handleDelete} />
       ))}  
     </div>
   );
@@ -21,7 +24,14 @@ FeedbackList.defaultProps = {
 };
 
 FeedbackList.propTypes = {
-  feedback: PropTypes.array,
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    }),
+  ),
+  handleDelete: PropTypes.func,
 };
 
 export default FeedbackList;
